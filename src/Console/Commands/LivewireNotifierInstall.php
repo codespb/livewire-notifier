9131c95b674c49a2152fd93f352cc3a0cdca0e30
Dima <cuamckuy@gmail.com>
2021-04-07 19:36:58 +0300

    Garbage collection

--- src/Console/Commands/LivewireNotifierInstall.php
+++ src/Console/Commands/LivewireNotifierInstall.php
@@ -7,10 +7,10 @@ use Illuminate\Console\Command;
 class LivewireNotifierInstall extends Command
 {
     /**
-         * The name and signature of the console command.
-         *
-         * @var string
-         */
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
     protected $signature = 'livewire-notifier:install';
 
     /**
@@ -28,28 +28,28 @@ class LivewireNotifierInstall extends Command
     public function handle()
     {
         // $this->getOutput()->title('Livewire Notifier package');
-        $this->table(['Livewire Notifier'],[
+        $this->table(['Livewire Notifier'], [
             ['Simple notifications system with zero dependencies above TALL-stack.'],
             ["\nVisit site:\nhttps://github.com/codespb/livewire-notifier"]
         ]);
         $this->info('😎 Installing Livewire Notifier…');
-        if(!strpos(file_get_contents('./composer.json'),'livewire/livewire')){
+        if (!strpos(file_get_contents('./composer.json'), 'livewire/livewire')) {
             $this->comment('Please, make sure that livewire/livewire package is installed!');
             return 1;
         }
         $tailwind_config_path = './tailwind.config.js';
-        if(!file_exists($tailwind_config_path)){
+        if (!file_exists($tailwind_config_path)) {
             $this->comment('Please, make sure that Tailwind CSS is installed and tailwind.config.js file is in the project root folder!');
             return 1;
         } else {
             $tailwind_config = file_get_contents($tailwind_config_path);
-            if(!strpos($tailwind_config, 'livewire-notifier')) {
+            if (!strpos($tailwind_config, 'livewire-notifier')) {
                 $tailwind_config = preg_replace("#purge:\s*\[\n(.+)\]#imsU", "purge: [\n        \"./vendor/livewire-notifier/**/*.blade.php\",\n$1]", $tailwind_config);
-                file_put_contents($tailwind_config_path,$tailwind_config);
+                file_put_contents($tailwind_config_path, $tailwind_config);
             }
         }
-        $this->call('vendor:publish',['--tag'=>'livewire-notifier.config']);
-        $this->call('vendor:publish',['--tag'=>'livewire-notifier.views']);
+        $this->call('vendor:publish', ['--tag'=>'livewire-notifier.config']);
+        $this->call('vendor:publish', ['--tag'=>'livewire-notifier.views']);
         $this->info('🥳 Livewire Notifier is installed!');
         return 0;
     }
